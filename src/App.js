15067aad878cf638b0510eba353a3fadc1168143
MaksimLisovoi <lisovoi123@gmail.com>
2021-02-23 17:33:39 +0200

    Добавляю второй компонент

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Profile from "./components/Profile/Profile";
 import user from "./db/user.json";
+import Statistics from "./components/Statistics/Statistics";
+import statisticalData from "./db/statistical-data.json";
 
 function App() {
   return (
     <div className="App">
       <Profile data={user} />
+      <Statistics stats={statisticalData} title="UPLOAD STATS" />
     </div>
   );
 }
